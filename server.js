7c37f3acf0d4e737270cbb8829ba274f4a762650
jeanmw <jean.weatherwax@gmail.com>
2017-04-03 14:53:58 -0700

    solutions

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ var app = express(),
     router = express.Router();
 
 // set port to env or 3000
-var port = process.env.API_PORT || 3001;
+var port = process.env.PORT || 3001;
 
-//db config
-//ADD YOUR INFO HERE!
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds115738.mlab.com:15738/mern-comment-box');
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -45,12 +42,53 @@ router.route('/nuke').get(function(req,res){
   });
 });
 
-//add /comments route to our /api router here
+//add /comments route to our /api router
+router.route('/comments')
+  // GET index
+  .get(function(req,res) {
+    //get all comments
+    Comment.find(function(err, comments){
+      if(err){ res.send(err)}
+      // send comments
+      res.json(comments);
+    });
+  })
+  .post(function(req, res) {
+    var comment = new Comment();
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if(err){res.send(err)}
+      res.json({ message: 'Comment successfully added!'});
+    });
+  });
+
+router.route('/comments/:comment_id')
+  .put(function(req, res) {
+    console.log("to update: ", req.params.comment_id);
+    Comment.findById(req.params.comment_id, function(err, comment){
+      if(err){res.send(err)}
+      (req.body.author) ? comment.author = req.body.author : null;
+      (req.body.text) ? comment.text = req.body.text : null;
+
+      comment.save(function(err) {
+        if(err){res.send(err)}
+        res.json({message: 'Comment has been updated'});
+      });
+    });
+  })
+  .delete(function(req, res){
+    Comment.remove({_id: req.params.comment_id}, function(err, comment){
+      if(err){res.send(err)}
+      res.json({ message: 'Comment has been deleted' })
+    })
+  });
 
 //use router config when we call /API
-app.use('/api', router);
+app.use('https://mern-comment-box-api.herokuapp.com/comments', router);
 
 //start server
-app.listen(port, function() {
+app.listen(process.env.PORT, function() {
   console.log(`api running on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
-//more React code here!
+ReactDOM.render(
+  <CommentBox
+    url='https://mern-comment-box-api.herokuapp.com/comments' pollInterval={2000} />,
+  document.getElementById('root')
+);
